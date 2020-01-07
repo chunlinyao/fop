6437317f91692874e793b06f0e9c2e064609f1ea
Yao Chunlin <chunlinyao@gmail.com>
2020-01-07 23:11:18 +0800

    vertical writing mode for japanese.
    (cherry picked from commit 1d23e51be340fa478fa3dabf764e5f4fa48ce72d)

--- fop-core/src/main/java/org/apache/fop/render/java2d/CustomFontMetricsMapper.java
+++ fop-core/src/main/java/org/apache/fop/render/java2d/CustomFontMetricsMapper.java
@@ -256,9 +256,9 @@ public class CustomFontMetricsMapper extends Typeface implements FontMetricsMapp
     /**
      * {@inheritDoc}
      */
-    public int[][] performPositioning(CharSequence cs, String script, String language, int fontSize) {
+    public int[][] performPositioning(CharSequence cs, String script, String language, int fontSize, boolean isVertical) {
         if (getRealFont() instanceof Positionable) {
-            return ((Positionable) typeface).performPositioning(cs, script, language, fontSize);
+            return ((Positionable) typeface).performPositioning(cs, script, language, fontSize, isVertical);
         } else {
             return null;
         }
@@ -267,9 +267,9 @@ public class CustomFontMetricsMapper extends Typeface implements FontMetricsMapp
     /**
      * {@inheritDoc}
      */
-    public int[][] performPositioning(CharSequence cs, String script, String language) {
+    public int[][] performPositioning(CharSequence cs, String script, String language, boolean isVertical) {
         if (getRealFont() instanceof Positionable) {
-            return ((Positionable) typeface).performPositioning(cs, script, language);
+            return ((Positionable) typeface).performPositioning(cs, script, language, isVertical);
         } else {
             return null;
         }
@@ -290,9 +290,9 @@ public class CustomFontMetricsMapper extends Typeface implements FontMetricsMapp
      * {@inheritDoc}
      */
     public CharSequence performSubstitution(CharSequence cs, String script, String language, List associations,
-                                            boolean retainControls) {
+                                            boolean retainControls, boolean isVertical) {
         if (getRealFont() instanceof Substitutable) {
-            return ((Substitutable) typeface).performSubstitution(cs, script, language, associations, retainControls);
+            return ((Substitutable) typeface).performSubstitution(cs, script, language, associations, retainControls, isVertical);
         } else {
             return cs;
         }
@@ -302,9 +302,9 @@ public class CustomFontMetricsMapper extends Typeface implements FontMetricsMapp
      * {@inheritDoc}
      */
     public CharSequence reorderCombiningMarks(CharSequence cs, int[][] gpa,
-        String script, String language, List associations) {
+                                              String script, String language, List associations, boolean isVertical) {
         if (getRealFont() instanceof Substitutable) {
-            return ((Substitutable) typeface).reorderCombiningMarks(cs, gpa, script, language, associations);
+            return ((Substitutable) typeface).reorderCombiningMarks(cs, gpa, script, language, associations, isVertical);
         } else {
             return cs;
         }
